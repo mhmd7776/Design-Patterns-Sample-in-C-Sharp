9d28f8a66a829c7fe989ed2c31d2142a6e043bca
mohammad mahdavi <mhmd7776@yahoo.com>
2022-12-16 18:28:39 +0330

    add singleton pattern

---
+++ Singleton/ConfigurationManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ConfigurationManager
+    {
+        private readonly Dictionary<string, object> _configurations = new ();
+        private static readonly ConfigurationManager Instance = new ();
+
+        private ConfigurationManager()
+        {
+                
+        }
+
+        public static ConfigurationManager GetConfiguration()
+        {
+            return Instance;
+        }
+
+        public void AddConfig(string key, object value)
+        {
+            _configurations.Add(key, value);
+        }
+
+        public object GetConfig(string key)
+        {
+            return _configurations[key];
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿using Singleton;
+
+var configurationManager = ConfigurationManager.GetConfiguration();
+configurationManager.AddConfig("SiteName", "Bugfixer");
+
+Console.WriteLine(configurationManager.GetConfig("SiteName"));
+
+var otherConfigurationManager = ConfigurationManager.GetConfiguration();
+
+Console.WriteLine(otherConfigurationManager.GetConfig("SiteName"));
